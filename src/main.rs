210222c5dba1b5002a6a8371ec1eec9aef813fa9
sjagadee <srini.jagadeesh@gmail.com>
2024-08-02 19:55:31 -0500

    basic code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn add_five(num: u32) -> u32 {
+    return num + 5;
+}
+
+// everyting is defualted to immutable
 fn main() {
-    println!("Hello, world!");
+    let mut x: u32 = 50;
+    println!("x is {}", x);
+
+    let y: u32 = add_five(x);
+    println!("y is {}", y);
+
+    x = 70;
+    println!("x is {}", x);
 }
